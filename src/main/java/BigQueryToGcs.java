8a627c67284b4e7d047c3d329ad8958966888599
Ashish-Ranjan-Srivastava <47782446+Ashish-Ranjan-Srivastava@users.noreply.github.com>
2021-07-13 20:05:38 +0530

    Updated Source Code

--- src/main/java/BigQueryToGcs.java
+++ src/main/java/BigQueryToGcs.java
@@ -1,7 +1,6 @@
 
 
 import org.apache.beam.sdk.Pipeline;
-import org.apache.beam.sdk.PipelineResult;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.ValueProvider;
@@ -11,25 +10,22 @@ import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
 import com.google.api.services.bigquery.model.TableRow;
 import org.apache.beam.sdk.extensions.jackson.AsJsons;
+import java.util.logging.Level;
 import java.util.logging.Logger;
-
 
 
 /*
 This script will load data from bigquery table
         and load into GCS Bucket in Json Format
-
  Parameters Required :-
     ProjectName - Your Project ID
     DatasetName - BigQuery Dataset Name
     Table - Table from where data is supposed to Export
     storageLocation - Destination Path (GCS Bucket e.g - gs://BUCKET NAME/DIR/)
-
     We need to explicitly set the tempLocation for beam runner to some temporary file
-
  */
 
-public class test {
+public class BigQueryToGcs {
     // Setting up logger for printing console message or to debug
 
     private final static Logger LOGGER =
@@ -37,9 +33,9 @@ public class test {
 
     // Assigning Environmental Variable
 
-    public interface inputOption extends PipelineOptions {
-         @Description("Setting Up Dataset Name")
-         ValueProvider<String> getDataset();
+    public interface InputOptions extends PipelineOptions {
+        @Description("Setting Up Dataset Name")
+        ValueProvider<String> getDataset();
         void setDataset(ValueProvider<String> value);
 
         @Description("Setting Up Table Name")
@@ -59,8 +55,8 @@ public class test {
 
     public static void main(String[] args)
     {
-        inputOption options = PipelineOptionsFactory.fromArgs(args).withValidation()
-                .as(inputOption.class);
+        InputOptions options = PipelineOptionsFactory.fromArgs(args).withValidation()
+                .as(InputOptions.class);
 
         Pipeline pipeline = Pipeline.create(options);
         // Explicitly providing temp location (by default it is mandatory argument for
@@ -75,16 +71,23 @@ public class test {
 
                 );
 
-
+ /*
+      PCollection<TableRow> record = rows.apply(ParDo.of(new DoFn<TableRow, TableRow>() {
+            @ProcessElement
+            public void ProcessElement(ProcessContext c)
+            {
+                c.output(c.element());
+            }
+        }));
+  */
 
 
         /* Converting PCollection<TableRow> into json format */
         PCollection<String> jsonForm = rows.apply("JsonTransform", AsJsons.of(TableRow.class));
 
-
-
+        LOGGER.log(Level.INFO, "Loading data into bucket");
         jsonForm.apply(TextIO.write().to(options.getStorageLocation()));
-        PipelineResult result = pipeline.run();
+        pipeline.run();
 
     }
 
